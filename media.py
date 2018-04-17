8c69f1ce93d66e1ffaa6bc4c1092093f123ff7b0
Dan <dan@Dan.local>
2018-04-17 18:40:57 +0100

    corrected python code style

--- media.py
+++ media.py
@@ -1,13 +1,16 @@
-import webbrowser #imports web browser controller
+import webbrowser  # imports web browser controller
 
-#creates movie class
+
+# creates movie class
 class Movie ():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title,
+                 movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
-        self.poster_image_url= poster_image
+        self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-#opens youtube trailer in browser
+# opens youtube trailer in browser
     def play_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
+
