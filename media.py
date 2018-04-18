790ea35c8760a0d67b507ee370068f9d4eebed16
Dan <dan@Dan.local>
2018-04-18 23:05:01 +0100

    corrected style

--- media.py
+++ media.py
@@ -1,9 +1,24 @@
 import webbrowser  # imports web browser controller
 
 
-# creates movie class. It contains the attributes common to all movie objects
-# and the method to open the trailer video in browser
 class Movie ():
+    """Class containing the attributes common to all movie objects.
+
+Args:
+    movie_title (str): movie title.
+    movie_storyline (str): storyline summary.
+    poster_image (str): url to the movie's poster image.
+    trailer_youtube (str): url to the movie trailer on YouTube.
+
+Attributes:
+    movie_title (str): movie title.
+    movie_storyline (str): storyline summary.
+    poster_image (str): url to the movie's poster image.
+    trailer_youtube (str): url to the movie trailer on YouTube.
+
+Methods:
+    play_trailer (str): opens a window on page playing the youtube video.
+"""
     def __init__(self, movie_title,
                  movie_storyline, poster_image, trailer_youtube):
         self.title = movie_title
